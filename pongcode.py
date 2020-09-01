c211cbf60160c7105a898f593c57a610bac3acdb
Jessica Murray <Jessicavmurray47@gmail.com>
2020-08-31 17:18:24 -0700

    save changes

--- pongcode.py
+++ pongcode.py
@@ -1,1 +1,9 @@
 #this is my pong code
+import turtle
+window=turtle.Screen()
+window.title("Pong by Jessica")
+window.bgcolor('green')
+window.setup(width=800, height=600)
+window.tracer(0)
+while True:
+    window.update()
